91838360ee4d14207ee562b7352e1c41a2ab732b
Johan2403 <johanemman243@gmail.com>
2020-11-09 20:26:56 +0530

    Created a new command

---
+++ src/main/java/johan24/HelloSpigot/HelloSpigot.java
@@ -0,0 +1,26 @@
+package johan24.HelloSpigot;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HelloSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(label.equalsIgnoreCase("Hello")) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&1H&2e&3l&4l&5o&6!"));
+        }
+        return false;
+    }
+}
